7cdeab597d13c035bd2cd7f17c49f1ae2f1526bf
qiutian00 <1476102147@qq.com>
2024-04-06 16:55:29 +0800

    fix: add test

--- tests/test.rs
+++ tests/test.rs
@@ -7,4 +7,28 @@ mod test {
 
         assert_eq!("Hello, world!", hello)
     }
+
+    #[test]
+    fn goodbye() {
+        let goodbye = "Goodbye, world!";
+        println!("Goodbye, world!");
+
+        assert_eq!("Goodbye, world!", goodbye)
+    }
+
+    // test directory
+    // ├── test1
+    // │   ├── test1-1
+    // │   └── test1-2
+    // └── test2
+    //     ├── test2-1
+    //     └── test2-2
+    #[test]
+    fn test_dir() {
+        let test_dir = "test directory";
+        println!("test directory");
+
+        assert_eq!("test directory", test_dir)
+    }
+
 }
